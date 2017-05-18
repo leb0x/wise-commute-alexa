40e61ef4b3884311a2d962d06087616307b63809
znergy <Guest@Epicodus-1E.local>
2017-05-18 16:23:18 -0700

    add working alexa skill ccode

---
+++ example.js
@@ -0,0 +1,362 @@
+var Alexa = require('alexa-sdk');
+var moment = require('moment'); // deals with dates and date formatting, for instance converts AMAZON.DATE to timestamp
+
+// import pension calculator
+var calculator = require('./pensionCalculator');
+
+var GENDERCUTOFFDATE = new Date(1953, 12, 6);
+
+// States are required for conversational skills.
+// States assume a context. e.g. _DOB expects date of birth; providing a gender in this state would confuse Alexa.
+// UX design principle have to be as unambiguous as possible in language (e.g. "are you male or female" vs "what gender are you?")
+var states = {
+    START: '_STARTMODE',  // Prompt the user to start or restart
+    DOB: '_DOB',
+    GENDER: '_GENDER',
+    ANSWER: '_ANSWER'
+};
+
+
+// Outbound messages spoken back to end user.
+// alexa-nodejs-sdk wraps all strings in the advanced SSML speech markup (<speak>STRING HERE</speak>) that allows phonetic overrides etc.
+var snippets = {
+            WELCOME: "<s>Welcome to the D W P Pension Age calculator.</s> " +
+            "<s>You can ask to calculate your pension age or for the U K pension eligibility criteria?</s>",
+
+            WELCOME_REPROMPT: "You can say, " +
+            "Calculate my pension age or, say what are the eligibility criteria in the U K.",
+
+            GENDER: "Thank you. Are you female or male?",
+
+            GENDER_REPROMPT: "In order to calculate your pension age, please tell me: Are you male or female?",
+
+            GENDER_INVALID: "Sorry I couldn't understand your gender, can you please tell me if you are you male or female?",
+
+            DATEOFBIRTH: "Ok, please tell me what is your date of birth?",
+
+            DATEOFBIRTH_REPROMPT: "In order to calculate your pension age please tell me your date of birth?",
+
+            DATEOFBIRTH_INVALID_FUTURE: "Nice you're from the future. Did you bring a hoverboard? Seriously, can you please say your actual date of birth please?",
+
+            DATEOFBIRTH_INVALID: "Please say your date of birth. For example you can say, my date of birth is the 23rd of April 1980",
+
+            STOP: "Thank you for using the D W P pension calculator.",
+
+            HELP: "You can ask things like: What is my pension age or what are the eligibility criteria.",
+
+            HELP_REPROMPT: "Simply say: calculate pension or eligibility criteria.",
+
+            UNHANDLED: "I'm sorry I couldn't understand what you meant. Can you please say it again?"
+};
+
+
+// You define a set of state handlers for every state PLUS the new session / launch event.
+var newSessionHandlers = {
+
+    // session variables stored in this.attributes
+    // session state is stored in this.handler.state
+    // handler.state vs Intent vs
+    'LaunchRequest': function() {
+        // Initialise State
+        this.handler.state = states.START;
+
+        // emitWithState should be called executeStateHandler("Start").
+        // As such this will call a handler "Start" in the startStateHandlers object.
+        // Maybe this line and the previous line could be more coherently wrapped into a single
+        // function:
+        // this.stateTransition( states.START, "Start" )
+        this.emit("Start")
+    },
+
+    // It's unclear whether this can ever happen as it's triggered by Alexa itself.
+    "Unhandled": function () {
+        var speechText = "I wasn't launched yet";
+        this.emit(":ask", speechText);
+    }
+};
+
+
+
+// This is the beginning of our skill.
+// This is a list of accepted intents Alexa is listening for
+// when the skill has just started.
+// In this specific version, a user can't provide things like date of birth
+// or gender as part of the initial skill invocation because we've not included in this set of start state handlers.
+// We could but haven't in this particular scenario.
+var startStateHandlers = Alexa.CreateStateHandler(states.START, {
+    'Start': function() {
+
+
+        this.attributes['dob'] = '';
+        this.attributes['gender'] = '';
+
+
+        this.handler.state = states.START;
+        var speechText = snippets.WELCOME;
+        var repromptText = snippets.WELCOME_REPROMPT;
+
+        // emit is the exit point to instruct Alexa how to and what to communicate with end user.
+        // e.g. do we want further information? (:ask) / no further information, skill terminates (:tell)
+        // do we provide a voice response with or without a card on the mobile device (:ask vs :askWithCard)
+        // https://github.com/alexa/alexa-skills-kit-sdk-for-nodejs
+        // as we've said :ask we are expecting the user to provide more information.
+        // maybe this function could be called this.respond()
+        // this is going to speak the snippets.WELCOME which implicitly asks a question (hence :ask).
+        // reprompt text is automatically spoken after a few seconds. This is a feature of the NodeJS SDK.
+        // See Unhandled for the fallback / unrecognised utteranes.
+        this.emit(':ask', speechText, repromptText);
+    },
+
+    // the intent text is defined in the
+    // Alexa interaction model web page at developer.amazon.com/ask
+    // represented as sample utterances.
+    'StartCalculationIntent': function () {
+        var speechText = snippets.DATEOFBIRTH;
+        var repromptText = snippets.DATEOFBIRTH_REPROMPT;
+
+        // Change State to calculation
+        this.handler.state = states.DOB;
+        this.emit(':ask', speechText, repromptText);
+    },
+
+    // a predefined Utterance that you don't need to define in your interaction model
+    // We are choosing to provide this help function but equally you don't need to.
+    "AMAZON.HelpIntent": function () {
+        var speechText = snippets.HELP;
+        var repromptText = snippets.HELP_REPROMPT;
+        this.emit(':ask', speechText, repromptText);
+    },
+    "Unhandled": function () {
+        var speechText = snippets.UNHANDLED;
+        this.emit(":ask", speechText);
+    },
+
+    // User says stop. Stops even in the middle of a response.
+    "AMAZON.StopIntent": function () {
+        var speechText = snippets.STOP;
+        this.emit(":tell", speechText);
+    },
+
+    // unclear really what the difference is; default working practice is
+    // to do the same thing
+    // in a production system we'd probably dedupe this function.
+    "AMAZON.CancelIntent": function () {
+        var speechText = snippets.STOP;
+        this.emit(":tell", speechText);
+    },
+    "AMAZON.StartOverIntent": function () {
+        this.emit("Start")
+    },
+
+    // TODO determine when this is requested and what initiates it
+    // Implement handler to save state if state should be stored persistently e.g. to DynamoDB
+    // 'SessionEndedRequest': function () {
+    //     console.log('session ended!');
+    //     this.emit(':saveState', true);
+    // }
+
+    // TODO add 'AMAZON.RepeatIntent' that repeats the last question.
+
+});
+
+
+
+var dobStateHandlers = Alexa.CreateStateHandler(states.DOB, {
+
+    'DateOfBirthIntent': function () {
+
+        var speechText = "",
+            repromptText = "";
+
+        var date_string = this.event.request.intent.slots.dob.value;
+        var date = moment(date_string);
+
+        if (date.isValid()) {
+
+            if (!isFutureDate(date)) {
+                // ALL GOOD – dob not in the future
+                speechText = snippets.GENDER;
+                repromptText = snippets.GENDER_REPROMPT;
+                this.attributes['dob'] = date_string;
+
+                if(isGenderNeeded(date)) {
+                    // Transition to next state
+                    this.handler.state = states.GENDER;
+                    // this.emit(":saveState", false);
+                    this.emit(':ask', speechText, repromptText);
+
+                } else {
+                    // gender not needed
+                    // this.attributes['gender'] = "unisex";
+                    this.handler.state = states.ANSWER;
+                    // this.emit(":saveState", false);
+                    this.emit("Answer")
+
+                }
+
+
+
+            } else {
+                // dob in the future
+                speechText = snippets.DATEOFBIRTH_INVALID_FUTURE;
+                // this.emit(":saveState", false);
+                repromptText = snippets.DATEOFBIRTH_INVALID_FUTURE; // could be improved by using alternative prompt text
+                this.emit(':ask', speechText, repromptText);
+            }
+
+        } else {
+            // not a valid Date
+            speechText = snippets.DATEOFBIRTH_INVALID;
+            // this.emit(':saveState', false);
+            repromptText = snippets.DATEOFBIRTH_INVALID; // could be improved by using alternative prompt text
+            this.emit(':ask', speechText, repromptText);
+        }
+    },
+    "AMAZON.HelpIntent": function () {
+        var speechText = snippets.HELP;
+        var repromptText = snippets.HELP_REPROMPT;
+        this.emit(':ask', speechText, repromptText);
+    },
+    "Unhandled": function () {
+        var speechText = snippets.UNHANDLED;
+        this.emit(":ask", speechText);
+    },
+    "AMAZON.StopIntent": function () {
+        var speechText = snippets.STOP;
+        this.emit(":tell", speechText);
+    },
+    "AMAZON.CancelIntent": function () {
+        var speechText = snippets.STOP;
+        this.emit(":tell", speechText);
+    },
+    "AMAZON.StartOverIntent": function () {
+        this.emit("Start")
+    },
+    'SessionEndedRequest': function () {
+        // this.emit(':saveState', false);
+        // this.attributes['dob'] = date_string;
+        // this.attributes['dob'] = date_string;
+        console.log('session ended!');
+    }
+});
+
+
+var genderStateHandlers = Alexa.CreateStateHandler(states.GENDER, {
+    'GenderIntent': function () {
+        var speechText = "",
+            repromptText = "";
+
+        var gender = this.event.request.intent.slots.gender.value;
+
+        if (isGenderSlotValid(gender)) {
+            // valid gender
+            this.attributes['gender'] = gender;
+            this.handler.state = states.ANSWER;
+
+            this.emit(':saveState', false);
+            this.emit("Answer");
+        } else {
+            // not a valid gender
+            speechText = snippets.GENDER_INVALID;
+            repromptText = snippets.GENDER_INVALID; // could be improved by using alternative prompt text
+
+            this.emit(':saveState', false);
+            this.emit(':ask', speechText, repromptText);
+        }
+    },
+    "AMAZON.HelpIntent": function () {
+        var speechText = snippets.HELP;
+        var repromptText = snippets.HELP_REPROMPT;
+        this.emit(':ask', speechText, repromptText);
+    },
+    "Unhandled": function () {
+        var speechText = snippets.UNHANDLED;
+        this.emit(":ask", speechText);
+    },
+    "AMAZON.StopIntent": function () {
+        var speechText = snippets.STOP;
+        this.emit(":tell", speechText);
+    },
+    "AMAZON.CancelIntent": function () {
+        var speechText = snippets.STOP;
+        this.emit(":tell", speechText);
+    },
+    "AMAZON.StartOverIntent": function () {
+        this.emitWithState("Start")
+    },
+    'SessionEndedRequest': function () {
+        this.emit(':saveState', false);
+        console.log('session ended!');
+    }
+});
+
+
+
+var answerStateHandlers = Alexa.CreateStateHandler(states.ANSWER, {
+    'Answer': function () {
+        console.log(`##### START INPUT SNIPPETS #####`);
+        // var dob =
+        // var gender = ;
+
+        console.log(`this.attributes.dob: "${this.attributes.dob}"`);
+        console.log(`this.attributes.gender: "${this.attributes.gender}"`);
+
+        var pensionDate = calculator.calculatePension(this.attributes.dob, this.attributes.gender);
+        console.log(`pensionDate: "${pensionDate}"`);
+
+        var speechText = calculator.createPensionSnippet(pensionDate);
+
+        // Change State to Start again
+        this.handler.state = states.START;
+        this.emit(':tell', speechText);
+
+        console.log(`##### STOP SNIPPET #####`);
+        console.log(``);
+
+    },
+    "Unhandled": function () {
+        var speechText = snippets.UNHANDLED;
+        this.emit(":ask", speechText);
+    }
+});
+
+
+
+function isGenderSlotValid(gender) {
+    if (gender == "male" || gender == "female") {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+function isFutureDate(dob) {
+    var today = moment();
+
+    if (dob > today) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function isGenderNeeded(dob) {
+    return dob < GENDERCUTOFFDATE;
+}
+
+
+
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = process.env.appId;
+    // alexa.appId = "your skill ID"; // better store it as ENV variable at AWS Lambda
+    // alexa.resources = languageStrings;
+
+    // register intent handlers for each state + new session.
+    // Each state constrains possible intents Alexa is listening for.
+    // If you only have one handler you are context-free and cannot have state.
+    alexa.registerHandlers(newSessionHandlers, startStateHandlers, dobStateHandlers, genderStateHandlers, answerStateHandlers);
+    alexa.execute();
+};bv v
--- index.js
+++ index.js
@@ -3,30 +3,30 @@ var Alexa = require('alexa-sdk')
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
+    alexa.appId = process.env.appId;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
 var handlers = {
   'LaunchRequest': function () {
-    // Launch Request
-    console.log(`LAUNCH REQUEST`)
+    console.log('launch request started')
   },
-  'GetStopName': function () {
-    this.emit(':ask', 'What stop?', 'What stop do you want arrivals for?')
-    
-    this.emit('GetTrainName')
+  'CreateRequest': function () {
+    this.attributes['stopName'] = ''
+    this.emit(':ask', 'What Stop?', 'In order to get arrival information, I need your desired stop.')
   },
   'GetTrainName': function () {
-    this.emit(':ask', 'What train?')
-
-    this.emit('GetArrivalInformation')
+    this.attributes['stopName'] = this.event.request.intent.slots.StopName.value;
+    this.attributes['trainName'] = ''
+    this.emit(':ask', 'What train?', 'In order to get arrival information, I need your desired train.')
   },
-  'GetArrivalInformation': function () {
-    console.log('GetArrivalInformation: working')
-    var inputTrainName = "green line to clackamas";
-    var inputStopName = "gateway";
+  'ArrivalInformation': function () {
+    this.attributes['trainName'] = this.event.request.intent.slots.TrainName.value;
+    this.attributes['firstArrival'] = ''
 
+    var inputTrainName = this.attributes.trainName;
+    var inputStopName = this.attributes.stopName;
     var stopId;
 
     // mapping direction to stop id, using train name
@@ -35,15 +35,303 @@ var handlers = {
         "gateway": {
           "clackamas": 8347,
           "city center": 8370,
-          "beaverton": 1000
+          "beaverton": 8370,
+          "gresham": 8347,
+          "hillsboro": 8370,
+          "airport": 8347
         },
-        "rose quarter": {
-          "clackamas": 1234,
-          "city center": 1234,
-          "airport": 1234
-        }
+        "clackamas": {
+          "clackamas": 13132,
+          "city center": 13132
+        },
+        "fuller": {
+          "clackamas": 13130,
+          "city center": 13133
+        },
+        "lents": {
+          "clackamas": 13128,
+          "city center": 13135
+        },
+        "flavel": {
+          "clackamas": 13129,
+          "city center": 13134
+        },
+        "holgate": {
+          "clackamas": 13127,
+          "city center": 13136
+        },
+        "powell": {
+          "clackamas": 13126,
+          "city center": 13137
+        },
+        "division": {
+          "clackamas": 13125,
+          "city center": 13138
+        },
+        "main": {
+          "clackamas": 13124,
+          "city center": 13139
+        },
+        "hatfield": {
+          "hillsboro": 9849,
+          "gresham": 9848
+        },
+        "washington": {
+          "city center": 9841,
+          "gresham": 9841,
+          "hillsboro":9842
+        },
+        "fair": {
+          "city center": 9838,
+          "gresham": 9838,
+          "hillsboro": 9837
+        },
+        "hillsboro": {
+          "gresham": 9846,
+          "hillsboro":9845
+        },
+        "tuality": {
+          "city center": 9843,
+          "gresham": 9843,
+          "hillsboro":9844
+        },
+        "washington": {
+          "city center": 9841,
+          "gresham": 9841,
+          "hillsboro":9842
+        },
+        "hawthorn": {
+          "city center": 9839,
+          "gresham": 9839,
+          "hillsboro":9840
+        },
+        "central": {
+          "city center": 9856,
+          "gresham": 9846,
+          "hillsboro":9845
+        },
+        "orenco": {
+          "city center": 9835,
+          "gresham": 9835,
+          "hillsboro": 9836
+        },
+        "quatama": {
+          "city center": 9834,
+          "gresham": 9834,
+          "hillsboro": 9833
+        },
+        "willow": {
+          "city center": 9831,
+          "gresham": 9831,
+          "hillsboro": 9832
+        },
+        "elmonica": {
+          "city center": 9830,
+          "gresham": 9830,
+          "hillsboro": 9829
+        },
+        "merlo": {
+          "city center": 9828,
+          "gresham": 9828,
+          "hillsboro": 9827
+        },
+        "creek": {
+          "city center": 9822,
+          "gresham": 9822,
+          "hillsboro": 9819
+        },
+        "beaverton central max station": {
+          "city center": 9824,
+          "gresham": 9824,
+          "hillsboro": 9823
+        },
+        "millikan": {
+          "city center": 9826,
+          "gresham": 9826,
+          "hillsboro": 9825
+        },
+        "btc": {
+          "city center": 9821,
+          "gresham": 9821,
+          "hillsboro": 9818
+        },
+        "beaverton transit center": {
+          "city center": 9821,
+          "gresham": 9821,
+          "hillsboro": 9818
+        },
+        "sunset": {
+          "city center": 9969,
+          "gresham": 9969,
+          "hillsboro": 9626
+        },
+        "washington park": {
+          "city center": 10120,
+          "gresham": 10120,
+          "hillsboro": 10121
+        },
+        "goosehollow": {
+          "city center": 10118,
+          "gresham": 10118,
+          "hillsboro": 10117
+        },
+        "goose": {
+          "city center": 10120,
+          "gresham": 10120,
+          "hillsboro": 10121
+        },
+        "providence": {
+          "city center": 9758,
+          "gresham": 9758,
+          "hillsboro": 9758
+        },
+        "sw 6th": {
+          "city center": 10120,
+          "gresham": 10120,
+          "hillsboro": 10121
+        },
+        "102nd": {
+          "gresham": 8348,
+          "city center": 8369,
+          "hillsboro": 8369
+        },
+        "East 102nd": {
+          "gresham": 8348,
+          "city center": 8369,
+          "hillsboro": 8369
+        },
+        "122nd": {
+          "gresham": 8349,
+          "city center": 8368,
+          "hillsboro": 8368
+        },
+        "East 122nd": {
+          "gresham": 8349,
+          "city center": 8368,
+          "hillsboro": 8368
+        },
+        "148th": {
+          "gresham": 8350,
+          "city center": 8367,
+          "hillsboro": 8367
+        },
+        "East 148th": {
+          "gresham": 8350,
+          "city center": 8367,
+          "hillsboro": 8367
+        },
+        "162nd": {
+          "gresham": 8351,
+          "city center": 8366,
+          "hillsboro": 8366
+        },
+        "East 162nd": {
+          "gresham": 8351,
+          "city center": 8366,
+          "hillsboro": 8366
+        },
+        "172nd": {
+          "gresham": 8352,
+          "city center": 8365,
+          "hillsboro": 8365
+        },
+        "East 172nd": {
+          "gresham": 8352,
+          "city center": 8365,
+          "hillsboro": 8365
+        },
+        "181st": {
+          "gresham": 8353,
+          "city center": 8364,
+          "hillsboro": 8364
+        },
+        "rockwood": {
+          "gresham": 8354,
+          "city center": 8363,
+          "hillsboro": 8363
+        },
+        "ruby": {
+          "gresham": 8355,
+          "city center": 8362,
+          "hillsboro": 8362
+        },
+        "civic": {
+          "gresham": 13450,
+          "city center": 13449,
+          "hillsboro": 13449
+        },
+        "gresham city hall": {
+          "gresham": 8356,
+          "city center": 8361,
+          "hillsboro": 8361
+        },
+        "gresham central": {
+          "gresham": 8357,
+          "city center": 8360,
+          "hillsboro": 8360
+        },
+        "cleveland": {
+          "city center": 8359,
+          "hillsboro": 8359
+        },
+        "airport": {
+          "beaverton": 10579
+        },
+        "mount hood": {
+          "airport": 10576,
+          "beaverton": 10577
+        },
+        "cascade": {
+          "airport": 10574,
+          "beaverton": 10575
+       },
+        "park rose": {
+          "airport": 10572,
+          "beaverton": 10573
+      },
+      "south east park": {
+        "city center": 13720,
+        "milwaukie":  13720
+      },
+      "milwaukie": {
+        "city center": 13721,
+        "milwaukie": 13718
+      },
+      "south east tacoma": {
+        "city center": 13722,
+        "milwaukie": 13717
+      },
+      "south east bybee": {
+        "city center": 13723,
+        "milwaukie": 13716
+      },
+      "17th and holgate": {
+        "city center": 13724,
+        "milwaukie": 13715
+      },
+      "rhine": {
+        "city center": 13725,
+        "milwaukie": 13714
+      },
+      "clinton": {
+        "city center": 13726,
+        "milwaukie": 13713
+      },
+     "museum": {
+        "city center": 13727,
+        "milwaukie": 13712
+      },
+      "south waterfront": {
+        "city center": 13728,
+        "milwaukie": 13711
+      },
+      "lincoln": {
+        "city center": 13729,
+        "milwaukie": 13710
       }
     }
+  }
+
     // gets train color
     var trainColor = inputTrainName.split(' ')[0];
 
@@ -69,49 +357,36 @@ var handlers = {
           // return arrivals by color
           if (data.resultSet.arrival[i].shortSign.toLowerCase().includes(trainColor)) {
             arrivals.push(data.resultSet.arrival[i])
-            firstArrival = arrivals[0]
-            secondArrival = arrivals[1]
+            this.attributes['firstArrival'] = arrivals[0]
+            this.attributes['secondArrival'] = arrivals[1]
+            var seconds = ((this.attributes.firstArrival.estimated) - (data.resultSet.queryTime))
+            var date = new Date(seconds)
+            var minutes = date.getMinutes()
+            var hours = date.getHours()
+
+
+            var estimatedArrival = (this.attributes.firstArrival.estimated)
+            var date = new Date(0)
+            date.setUTCMilliseconds(estimatedArrival)
+            console.log(date)
+            var estimatedMinutes = date.getMinutes()
+            console.log(estimatedMinutes)
+            var estimatedHours = date.getHours()
+            console.log(estimatedHours)
+            var suffix = (estimatedHours >= 12) ? 'PM' : 'AM'
+            estimatedHours = (estimatedHours > 12) ? estimatedHours - 12 : estimatedHours
+            estimatedHours = (estimatedHours == '00') ? 12 : estimatedHours
+
+            var estimatedArrivalTime = estimatedHours + ':' + estimatedMinutes + ' ' + suffix
+
+
+            this.emit(':tell', 'The next arrival for ' + this.attributes.firstArrival.shortSign + ' at ' + this.attributes.stopName + ' will be in ' + minutes + ' minutes')
           }
         }
-        console.log(firstArrival)
-        console.log(secondArrival)
-
-        var trainName = firstArrival.shortSign;
-        var scheduledTime = firstArrival.scheduled;
-
-        context.succeed(
-            generateResponse(
-              buildSpeechletResponse(`The ${trainName}, will be arriving at ${inputStopName} in ${scheduledTime}`, true),
-              {}
-            )
-          )
       })
     })
   },
   'Unhandled': function() {
-    this.emit(':ask', 'Insert your own error message here');
+    this.emit(':ask', 'test');
    }
-};
-
-// Helper Functions
-buildSpeechletResponse = (outputText, shouldEndSession) => {
-
-  return {
-    outputSpeech: {
-      type: "PlainText",
-      text: outputText
-    },
-    shouldEndSession: shouldEndSession
-  }
-
-}
-
-generateResponse = (speechletResponse, sessionAttributes) => {
-
-  return {
-    version: "1.0",
-    sessionAttributes: sessionAttributes,
-    response: speechletResponse
-  }
-
 }
